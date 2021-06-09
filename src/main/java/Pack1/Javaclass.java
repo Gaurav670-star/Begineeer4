410c513dd5f2c6bcd38a9a8b25418688a9e50f92
Gaurav670-star <gaurav.kumar@zopsmart.com>
2021-06-09 21:15:38 +0530

    First Commit

--- src/main/java/Pack1/Javaclass.java
+++ src/main/java/Pack1/Javaclass.java
@@ -3,5 +3,7 @@ package Pack1;
 public class Javaclass {
     public static void main(String[] args){
         System.out.println("Papa");
+        System.out.println("LOVE");
+        System.out.println("LOVE");
     }
 }
